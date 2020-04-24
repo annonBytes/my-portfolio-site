dee982cae17db5c585fa6fcd4309a31328f6e980
annonBytes@gmail.com <beinmonyudaniel@gmail.com>
2020-04-24 22:23:22 +0100

    Adding the structure and install node-sass

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles/App.scss';
+
+function App() {
+  return (
+    <div className='App'>
+      <h2>Hello this is your starter files </h2>
+    </div>
+  );
+}
+
+export default App;
